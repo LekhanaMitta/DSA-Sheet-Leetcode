efe6b0759a826656d845c19dc2537ad23d9791db
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-07-03 00:04:29 +0000

    [LeetCode Sync] Runtime - 1279 ms (72.15%), Memory - 0.0B (100.00%)

---
+++ solutions/0607-sales-person/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT sp.name FROM Salesperson sp
+WHERE sp.name NOT IN (SELECT s.name FROM SalesPerson s
+ JOIN Orders o ON s.sales_id = o.sales_id
+ JOIN Company c ON c.com_id = o.com_id
+WHERE c.name = 'RED');
+
