d8f3a9af071e58761eb6c71dec0fa07a572f85c4
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-09-08 22:40:47 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.7 MB (67.51%)

---
+++ solutions/0091-decode-ways/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numDecodings(string s) 
+    {
+        int n = s.length();
+        vector<int> dp(n+1);
+        dp[0] = 1;
+        dp[1] = (s[0] != '0') ? 1:0;
+        for(int i=2;i<=n;i++)
+        {
+            if(s[i-2] == '1' || (s[i-2] == '2' && s[i-1] <= '6'))
+                dp[i] += dp[i-2];
+            if(s[i-1] >= '1' && s[i-1] <= '9')
+                dp[i] += dp[i-1];
+        }   
+        return dp[n]; 
+    }
+};
