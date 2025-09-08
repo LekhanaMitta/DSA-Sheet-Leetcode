65f6183d3efab9185edb001af85370d5b51310ab
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-09-08 04:14:23 +0000

    [LeetCode Sync] Runtime - 2445 ms (5.08%), Memory - 99.9 MB (64.59%)

---
+++ solutions/1700-minimum-time-to-make-rope-colorful/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minCost(string colors, vector<int>& neededTime) 
+    {
+        int cost = 0, i=0;
+        while(i < colors.size())
+        {
+            char curr = colors[i];
+            int mx = 0;
+            while(colors[i] == curr && i < colors.size())
+            {
+                cost += neededTime[i];
+                cout << cost << " ";
+                mx = max(mx, neededTime[i]);
+                i++;
+            }
+            cost -= mx;
+            cout << cost << " ";
+        } 
+        return cost;   
+    }
+};
