ca93cc6c9902f588fb4d021d9c4423da91de9a1b
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-09-12 01:34:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 12.7 MB (26.45%)

---
+++ solutions/0162-find-peak-element/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) 
+    {
+        int l = 1, r = nums.size()-2, n = nums.size();
+        if(n == 1)  return 0;
+        if(nums[0] > nums[1])   return 0;
+        if(nums[n-1] > nums[n-2])   return n-1;
+        while(l <= r)    
+        {
+            int m = (l+r)/2;
+            cout << m << " " << nums[m] << endl;
+            if(nums[m-1] < nums[m] && nums[m+1]<nums[m])
+                return m;
+            else if(nums[m-1]<nums[m] && nums[m]<nums[m+1])
+                l=m+1;
+            else
+                r=m-1;
+        }
+        return -1;
+    }
+};
