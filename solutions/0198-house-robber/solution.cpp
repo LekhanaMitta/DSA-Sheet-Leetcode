7e9d07d16896bc430fee9ace7a77283aef4949cd
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-09-26 00:28:24 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.1 MB (78.24%)

--- solutions/0198-house-robber/solution.cpp
+++ solutions/0198-house-robber/solution.cpp
@@ -2,17 +2,24 @@ class Solution {
 public:
     int rob(vector<int>& nums) 
     {
-        if(nums.size() == 1) return nums[0];
-        int first = nums[0];
-        if(nums.size() == 2) return max(nums[0], nums[1]);
-        int second = max(nums[0], nums[1]);
-        int third = 0;
-        for(int i = 2; i<nums.size();i++)
+        int n = nums.size();
+        // if(n == 1) return nums[0];
+        // if(n == 2) return max(nums[0], nums[1]);
+        // vector<int> dp(n+2, 0);
+        // // state : dp[i] - max money I can get starting from ith house without alerting police
+        // // recurrence: 
+        // // i) skip current house - max amount = dp[i+1] 
+        // // ii) rob current house - max amount = dp[i+2] + nums[i]
+        // for(int i = n-1;i>=0;i--)
+        //     dp[i] = max(dp[i+1], dp[i+2]+nums[i]);
+        // return dp[0];   
+        int first = 0, second = 0, ans = 0;
+        for(int i=n-1;i>=0;i--)
         {
-            third = max(first+nums[i], second);
-            first = second;
-            second = third;
+            ans = max(first, second + nums[i]);
+            second = first;
+            first = ans;
         }
-        return third;
+        return ans;
     }
 };
