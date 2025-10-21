6f682acfa0272199aa5e3d7e1038496844dd60c7
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-10-21 16:07:41 -0400

    2074. Reverse Nodes in Even Length Groups

---
+++ LinkedList/Medium/Leetcode_2074.cpp
@@ -0,0 +1,51 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseEvenLengthGroups(ListNode* head) 
+    {
+        if(head -> next == NULL || head -> next -> next == NULL)  return head;
+        int group = 1;
+        ListNode *temp = head;
+        while(temp && temp -> next)
+        {
+            int cnt = 0;
+            group++;
+            ListNode *temp1 = temp -> next;
+            while(temp1 && cnt < group)
+            {
+                temp1 = temp1 -> next;
+                cnt++;
+            }
+            if(cnt % 2 == 0)
+            {
+                ListNode *curr = temp -> next, *prev = NULL, *next = NULL;
+                for(int i = 0;i < cnt;i++)
+                {
+                    next = curr -> next;
+                    curr -> next = prev;
+                    prev = curr;
+                    curr = next;
+                }
+                ListNode *temp2 = temp -> next;
+                temp2 -> next = curr;
+                temp -> next = prev;
+                temp = temp2;
+            }
+            else
+            {
+                for(int i=0;i<cnt;i++)
+                    temp = temp -> next;
+            }
+        }
+        return head;
+    }
+};
