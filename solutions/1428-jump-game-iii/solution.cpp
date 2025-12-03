2d46cdaed07188ebeac97a7e9d3946dc2249db19
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-12-03 21:28:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 34.7 MB (96.24%)

---
+++ solutions/1428-jump-game-iii/solution.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) 
+    {
+        if(start >= arr.size() || start < 0 || arr[start] < 0)    return false;
+        if(arr[start] == 0)   return true;
+        arr[start] = -arr[start];
+        return (canReach(arr, start+arr[start]) || canReach(arr, start - arr[start])); 
+    }
+};
