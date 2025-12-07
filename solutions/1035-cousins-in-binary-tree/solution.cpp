419a308c0a1d808c1eb73fd7fe98a332c1425caa
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2025-12-07 22:51:23 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 14 MB (75.92%)

--- solutions/1035-cousins-in-binary-tree/solution.cpp
+++ solutions/1035-cousins-in-binary-tree/solution.cpp
@@ -11,47 +11,28 @@
  */
 class Solution {
 public:
-    int p;
-    // void dfs(TreeNode* root, int x)
-    // {
-    //     if(!root)   return;
-    //     if((root -> left && root -> left -> val == x )|| (root -> right && root -> right -> val == x)) 
-    //     {
-    //             p = root -> val;
-    //             return;
-    //     }
-    //     dfs(root -> left, x);
-    //     dfs(root -> right, x);
-    //     h++;
-    // }
-    int dfs(TreeNode* root, int x, int h)
+    int height(TreeNode* root, int node, int h)
     {
         if(!root)   return 0;
-        if((root -> left && root -> left -> val == x )|| (root -> right && root -> right -> val == x))
-        {
-            p = root -> val;
-            return h+1;
-        }
-        int left = dfs(root -> left, x, h+1);
+        if(root -> val == node) return h;
+        int left = height(root->left, node, h+1);
         if(left != 0)   return left;
-        int right = dfs(root -> right, x, h+1);
-        if(right != 0)  return right;
-        return 0;
+        return height(root -> right, node, h+1);
     }
-
+    bool sameParent(TreeNode* root, int x, int y)
+    {
+        if(!root)   return false;
+        if(root -> left && root -> right)
+        {
+            if((root->left->val == x && root->right->val == y) || (root->left->val == y && root->right->val == x))
+                return true;
+        }
+        return sameParent(root -> left, x, y) || sameParent(root -> right, x, y);
+    }
     bool isCousins(TreeNode* root, int x, int y) 
     {
-        int h1 = dfs(root, x, 1);
-        int p1 = p;
-        cout << h1 << " " << p1 << endl;
-        int h2 = dfs(root, y, 1);
-        int p2 = p;
-        cout << h2 << " " << p2 << endl;
-        if(h1 == h2)
-        {
-            if(p1 == p2)    return false;
-            else    return true;
-        }
-        return false;
+        return height(root, x, 0) == height(root, y, 0) && !sameParent(root, x, y);
     }
 };
+/*
+*/
