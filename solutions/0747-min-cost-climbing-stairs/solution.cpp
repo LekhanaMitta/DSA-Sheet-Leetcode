fb77efa341189f8664f2a337c2047f188e5c720c
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-01-09 18:38:25 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.6 MB (73.37%)

--- solutions/0747-min-cost-climbing-stairs/solution.cpp
+++ solutions/0747-min-cost-climbing-stairs/solution.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
+/*
+State : dp[i] --> minimum cost to reach ith step.
+Transition : To reach ith step we need to come from 
+i. i-1 step ==> coming from i-1th step it costs cost[i-1]
+ii. i-2 step ==> coming from i-2th step it costs cost[i-2]
+- We choose which ever costs minimun so 
+dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2])
+Base case : dp[0] = 0, dp[1] = 0
+Answer : dp[n]
+*/
     int minCostClimbingStairs(vector<int>& cost) 
     {
-        if(cost.size() == 2)    return min(cost[0], cost[1]); 
-        int n = cost.size();  
-        vector<int> dp(n+1, 0);
-        dp[0] = 0, dp[1] = 0, dp[2] = min(cost[0], cost[1]);
-        for(int i = 3;i<=cost.size();i++)
-            dp[i] = min(dp[i-2]+cost[i-2], dp[i-1]+cost[i-1]);
-        // cout << dp[0] << " " << dp[1] << " " << dp[2] << " " << dp[3];
-        return dp[n];
+        int n = cost.size();
+        // vector<int> dp(n+1,0);
+        // dp[1] = 0;
+        // for(int i=2;i<=n;i++)
+        //     dp[i] = min(dp[i-1]+cost[i-1], dp[i-2]+cost[i-2]);
+        // return dp[n];    
+
+        int older = 0, recent = 0;
+        for(int i=2;i<=n;i++)
+        {
+            int ans = min(older+cost[i-2], recent+cost[i-1]);
+            older = recent;
+            recent = ans;
+        }
+        return recent;
     }
 };
-// 10  15  20
-// 10  10  15
-        
-//         1   100 1   1   1   100 1   1   100 1
-// dp  0   1   1   2   2   3   3   4   5   5   6
-
-// dp[0] = 0
-// dp[1] = a[0]
-// dp[2] = min(a[1], dp[0]+a[0])
-// dp[i+1] = min(dp[i])
-// dp[i] = 
-// min(dp[i], dp[i-1]+cost[i])
