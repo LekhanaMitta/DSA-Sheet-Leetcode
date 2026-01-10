bf6bc3e2b9dbd953dc5bc1f0b9fa360780a2b0e1
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-01-10 21:52:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 10.2 MB (75.53%)

--- solutions/0213-house-robber-ii/solution.cpp
+++ solutions/0213-house-robber-ii/solution.cpp
@@ -1,22 +1,38 @@
 class Solution {
 public:
-    int func(int start, int end, vector<int> &nums)
-    {
-        int first = 0, second = 0, ans = 0;
-        for(int i=start;i<=end;i++)
-        {
-            ans = max(second, first + nums[i]);
-            first = second;
-            second = ans;
-        }
-        return ans;
-    }
     int rob(vector<int>& nums) 
     {
-        // state : dp[i] - 
         int n = nums.size();
         if(n == 1)  return nums[0];
-        // cout << func(0, n-2, nums) << " " << func(1, n-1, nums) << endl;
-        return max(func(0, n-2, nums), func(1, n-1, nums));    
+        // vector<int> dp(n+1,0);
+        // // Robbing 1st house and not robbing nth house(cost only until n-1 house)
+        // dp[1] =  nums[0];
+        // for(int i=2;i<n;i++)
+        //     dp[i] = max(dp[i-2]+nums[i-1], dp[i-1]);
+        // int x = dp[n-1]; // Maximum cost in method 1
+        // // Not robbing 1st house and robbing nth house(dp[1] = 0)
+        // dp[1] = 0;
+        // for(int i=2;i<=n;i++)
+        //     dp[i] = max(dp[i-2]+nums[i-1], dp[i-1]);
+        // int y = dp[n];// maximum cost in method 2
+        // // Compare cost through method 1 and method 2 return the max of it.
+        // return max(x,y);
+        int before_last = 0, last = nums[0];
+        for(int i=1;i<n-1;i++)
+        {
+            int ans = max(before_last + nums[i], last);
+            before_last = last;
+            last = ans;
+        }
+        int x = last;
+        before_last = 0, last = 0;
+        for(int i=1;i<n;i++)
+        {
+            int ans = max(before_last + nums[i], last);
+            before_last = last;
+            last = ans;
+        }
+        int y = last;
+        return max(x, y);    
     }
 };
