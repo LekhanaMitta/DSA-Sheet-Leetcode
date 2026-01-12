c3e46f4ddc3384987c5986be86a11d93152883c0
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-01-12 12:29:41 -0500

    Longest Common Substring
    Implement dynamic programming solution for longest common substring.

---
+++ DP/Medium/Leetcode_0000.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+/*
+State : dp[i][j] --> length of common subseq from text1[0...i] and text2[0...j]
+Transition : 
+i. if(text1[i-1] == text2[j-1] ==> dp[i][j] = dp[i-1][j-1] + 1)
+ii. else dp[i][j] = 0
+Base Case:
+dp[0][j] = 0, dp[i][0] = 0
+Answer : dp[m][n]
+*/
+    int longestCommonSubstring(string text1, string text2) 
+    {
+        int m = text1.length();
+        int n = text2.length();
+        vector<vector<int>> dp(m+1, vector<int>(n+1,0));
+        for(int i=1;i<=m;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+                if(text1[i-1] == text2[j-1])
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                else
+                    dp[i][j] = 0;
+            }
+        }    
+        return dp[m][n];
+    }
+};
