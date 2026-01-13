84c97eced37216f220c4ae525a180c175bc9b1df
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-01-13 12:53:01 -0500

    647. Palindromic Substrings
    Implement a solution to count palindromic substrings.

---
+++ DP/Medium/Leetcode_647.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+/*
+Similar to Longest Palindromic Substring with just a little modification of incrementing the count for every dp[i][j] becomes 1.
+*/
+    int countSubstrings(string s) 
+    {
+        int n = s.length();
+        vector<vector<int>> dp(n,vector<int>(n,0));
+        int cnt = 0;
+        for(int i=0;i<n;i++)
+        {
+            dp[i][i] = 1;
+            cnt++;
+        }
+        for(int len=2;len<=n;len++)
+        {
+            for(int i=0;i+len-1<n;i++)
+            {
+                int j = i+len-1;
+                if(s[i] == s[j])
+                {
+                    if(len == 2)
+                        dp[i][j] = 1;
+                    else
+                        dp[i][j] = dp[i+1][j-1];
+                }
+                if(dp[i][j])
+                    cnt++;
+            }
+        }
+        return cnt;   
+    }
+};
