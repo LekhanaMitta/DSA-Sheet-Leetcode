7dd7489cdea72671951335ad76691cedd0fb89b2
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-02-03 17:41:59 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 8.6 MB (19.91%)

--- solutions/0007-reverse-integer/solution.cpp
+++ solutions/0007-reverse-integer/solution.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
-// 2147483648
     int reverse(int x) 
     {
-        bool sign = (x < 0) ? false:true;
-        if(x == -2147483648)    return 0;
-        x = (x < 0) ? -x:x;
-        long long int ans = 0;
-        while(x > 0)
+        long long ans = 0;
+        bool sign = true;
+        if(x == INT_MAX || x == INT_MIN)    return 0;
+        if(x < 0)
         {
+            sign = false;
+            x = 0-x;
+        }
+        while(x)
+        {
+
             ans *= 10;
             ans += x%10;
             x /= 10;
         }  
-        if(sign && ans > 2147483647)    return 0;
-        if(!sign && ans > 2147483648)   return 0;
-        return sign ? ans:0-ans;  
+        ans = sign ? ans : -ans;
+        if (ans > INT_MAX || ans < INT_MIN)
+            return 0;
+        return (int)ans;
     }
 };
