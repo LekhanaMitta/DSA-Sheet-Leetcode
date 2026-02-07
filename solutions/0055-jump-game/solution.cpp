1c25c56acd622b440052e96c5ef6217ddcedbbdb
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-02-07 20:46:15 +0000

    [LeetCode Sync] Runtime - 3 ms (28.91%), Memory - 52.2 MB (72.27%)

--- solutions/0055-jump-game/solution.cpp
+++ solutions/0055-jump-game/solution.cpp
@@ -2,28 +2,12 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) 
     {
-        int n = nums.size();
-        vector<int> dp(n+1, false);
-        // state: dp[i] -> is it possible to reach ith step from index 0
-        // dp[0] = true;
-        // for(int i=0;i<n;i++)
-        // {
-        //     if(!dp[i])
-        //         return false;
-        //     for(int j=1;j<=nums[i];j++)
-        //         dp[min(i+j, n)] = dp[i];
-        //     // cout << dp[i] << " ";
-        // }    
-        // return dp[n-1];
-        int reach = 0;
-        // Greedy: if any index i is not reachable from previous indices return false
-        for(int i=0;i<n;i++)
+        int maxReach = 0;
+        for(int i=0;i<nums.size();i++)
         {
-            if(i > reach)
-                return false;
-            reach = max(reach, i+nums[i]);
+            if(i>maxReach)  return false;
+            maxReach = max(maxReach,i+nums[i]);
         }
-        return true;
-
+        return (maxReach >= nums.size()-1);
     }
 };
