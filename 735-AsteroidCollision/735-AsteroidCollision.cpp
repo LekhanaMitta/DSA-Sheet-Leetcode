42e1c6a1fddbd87d55734631772da08c41d47195
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-02-09 13:27:44 -0500

    Create 735-AsteroidCollision

---
+++ 735-AsteroidCollision/735-AsteroidCollision.cpp
@@ -0,0 +1,30 @@
+// Last updated: 2/9/2026, 1:27:44 PM
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) 
+    {
+        vector<int> ans; 
+        for(int a : asteroids)
+        {
+            bool alive = true;
+            while(alive && !ans.empty() && ans.back() > 0 && a < 0)
+            {
+                int top = ans.back();
+                if(top + a < 0)
+                {
+                    ans.pop_back();
+                }
+                else if(top + a == 0)
+                {
+                    ans.pop_back();
+                    alive = false;
+                }
+                else
+                    alive = false;
+            }
+            if(alive)
+                ans.push_back(a);
+        } 
+        return ans;  
+    }
+};
