1ac889d20c3cd173695324f45903b28c516a7f23
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-02-09 13:26:46 -0500

    Create 2274-KeepMultiplyingFoundValuesByTwo

---
+++ 2274-KeepMultiplyingFoundValuesByTwo/2274-KeepMultiplyingFoundValuesByTwo.cpp
@@ -0,0 +1,19 @@
+// Last updated: 2/9/2026, 1:26:45 PM
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) 
+    {
+        map<int, bool> m;
+        for(int i=0;i<nums.size();i++)
+            m[nums[i]] = true;
+        int flag = true;
+        while(flag)
+        {
+            if(m.find(original) != m.end())
+                original = 2 * original;
+            else
+                flag = false;
+        }  
+        return original;  
+    }
+};
