4b3f03b8b28a551d9a6262d3c5c09c73ed2699d4
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-02-12 16:53:56 -0500

    45. Jump Game II

---
+++ Greedy/Medium/Leetcode_45.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int jump(vector<int>& nums)
+    {
+        if(nums.size() <= 1)    return 0;
+        int steps = 0, maxReach = 0, end = 0;
+        for(int i=0;i<nums.size();i++)
+        {
+            maxReach = max(maxReach, nums[i] + i);
+            if(i == end)
+            {
+                steps++;
+                end = maxReach;
+                if(end >= nums.size()-1)
+                    return steps;
+            }
+        }  
+        return steps;
+    }
+};
