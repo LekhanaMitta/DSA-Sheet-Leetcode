ea73b985cbc500c782f4d70d89f6a57e04ac1421
Bhanu <58395086+LekhanaMitta@users.noreply.github.com>
2026-02-18 13:39:31 -0500

    78. Subsets

---
+++ Backtracking/Medium/Leetcode_78.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) 
+    {
+        vector<vector<int>> subs;
+        int n = nums.size();
+        for(int i=0;i<(1<<n);i++)
+        {
+            vector<int> temp = {};
+            for(int j=0;j<n;j++)
+            {
+                if(i & (1 << j))
+                    temp.push_back(nums[j]);
+            }
+            subs.push_back(temp);
+        } 
+        return subs;
+    }
+};
